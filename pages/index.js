f3b80a3f59d9d2975dfcb6d93685d9cdf4f423e3
Andrew Kuzmin <andrukuzminkuzmin@yandex.ru>
2022-10-20 23:11:10 +0300

    доделать кнопку редактирования профиля + шрифты + остальные комментарии ревьера

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 const openPopupButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const popupCloseButton = popup.querySelector('.popup__close-button')
-
+const formElement = popup.querySelector('.popup__form')
+let nameInput = popup.querySelector('.popup__input_name')
+let jobInput = popup.querySelector('.popup__input_description')
+let profileName = document.querySelector('.profile__name')
+let profileDescription = document.querySelector('.profile__description')
 
 openPopupButton.addEventListener('click', function () {
     popup.classList.add('popup_opened')
@@ -10,14 +14,6 @@ openPopupButton.addEventListener('click', function () {
 function popupClose() {
     popup.classList.remove('popup_opened')
 }
-popupCloseButton.addEventListener('click', popupClose)
-
-let formElement = popup.querySelector('.popup__form')
-
-let nameInput = popup.querySelector('.popup__input_name')
-let jobInput = popup.querySelector('.popup__input_description')
-let profileName = document.querySelector('.profile__name')
-let profileDescription = document.querySelector('.profile__description')
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -26,4 +22,5 @@ function formSubmitHandler (evt) {
     popupClose()
 }
 
+popupCloseButton.addEventListener('click', popupClose)
 formElement.addEventListener('submit', formSubmitHandler);
