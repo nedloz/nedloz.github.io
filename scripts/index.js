f441592417cc6dec130503a8f31f0f10a0053d99
Andrew Kuzmin <andrukuzminkuzmin@yandex.ru>
2022-10-22 23:48:16 +0300

    ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,10 @@ const openPopupButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const popupCloseButton = popup.querySelector('.popup__close-button')
 const formElement = popup.querySelector('.popup__form')
-let nameInput = popup.querySelector('.popup__input_name')
-let jobInput = popup.querySelector('.popup__input_description')
+const nameInput = popup.querySelector('.popup__input_type_name')
+const jobInput = popup.querySelector('.popup__input_type_description')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
-
-openPopupButton.addEventListener('click', function () {
-    popup.classList.add('popup_opened')
-})
 
 function popupClose() {
     popup.classList.remove('popup_opened')
@@ -21,6 +17,12 @@ function formSubmitHandler (evt) {
     profileDescription.textContent = jobInput.value
     popupClose()
 }
+
+openPopupButton.addEventListener('click', function () {
+    nameInput.value = profileName.textContent
+    jobInput.value = profileDescription.textContent
+    popup.classList.add('popup_opened')
+})
 
 popupCloseButton.addEventListener('click', popupClose)
 formElement.addEventListener('submit', formSubmitHandler);
